37ce8ec2aca8414a3e6c56e1b4cd662c0780acc2
Zack Siri <zack@codemy.net>
2015-10-23 13:18:26 +0700

    extracted aggregations into refiners

--- lib/refiner.rb
+++ lib/refiner.rb
@@ -1,5 +1,43 @@
+require "rails"
 require "refiner/version"
 
 module Refiner
-  # Your code goes here...
+  module ViewHelpers
+    REFINER_VALUE = { 
+      remove:  -> (query, scope, slug) { (query[scope].split(/,/) - [slug.to_s]).join(',')  },
+      merge:   -> (query, scope, slug) { (query[scope] || "").split(/,/).push(slug.to_s).uniq.join(',') },
+      replace: -> (query, scope, slug) { slug.to_s }
+    }
+
+    def refiner_path(scope, slug, type)
+      merged_query = refiners type, scope.to_s, slug
+      filter_path = merged_query.keys.map { |key| [key, merged_query[key]] }.join('/')
+      merged_query.present? ? search_movies_path(filter_path) : movies_path
+    end
+
+    def refiner_active? scope, slug
+      (query[scope.to_s] || "").split(/,/).include?(slug.to_s) ? 'active' : nil
+    end
+
+    def refiner_value action, scope, slug
+      refiner_VALUE[refiner_value_selector(action, scope, slug)].call(query, scope, slug)
+    end
+
+    def refiner_value_selector action, scope, slug
+      refiner_active?(scope, slug).nil? ? action : :remove
+    end
+
+    def refiners action, scope, slug
+      merge_values = refiner_value action, scope, slug
+      if merge_values.present?
+        query.merge({ scope => merge_values })
+      else
+        query.delete_if { |k, v| k == scope }
+      end
+    end
+  end
+
+  class Engine < ::Rails::Engine
+    ActionView::Base.send :include, Refiner::ViewHelpers
+  end
 end
